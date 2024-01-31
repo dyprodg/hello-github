4953b64e4362d6ed608cbfe16608767738294b79
Dennis <dennisdiepolder@outlook.com>
2024-01-31 12:38:01 +0100

    test

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # Python code
 import os
 
-def greet(name):
-    return f"Hello, from GithubAction {name}"
-
 def greet():
     name = os.getenv('NAME', 'GitHub Copilot')
     return f"Hello, from GithubAction {name}"
